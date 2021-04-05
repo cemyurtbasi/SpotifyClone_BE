2027e7e4a5e91cd8588e102d95b181384c224977
cemyurtbasi <cemyurtbasi@gmail.com>
2021-04-06 01:05:13 +0300

    Service rework to async await

--- server.js
+++ server.js
@@ -22,151 +22,103 @@ app.use((req, res, next) => {
 });
 
 //Middleware END
-const spotifyDefaults = () => {
+const spotifyDefaults = (headers) => {
   return {
-    redirectUri:
-      process.env.NODE_ENV === "production"
-        ? "/play/"
-        : "http://localhost:3000/play/",
+    redirectUri: `${headers.origin}/play/`,
     clientId: process.env.CLIENT_ID,
     clientSecret: process.env.CLIENT_SECRET,
   };
 };
 
-app.post("/api/v1/refresh", (req, res) => {
-  const refreshToken = req.body.refreshToken;
-  console.log({...spotifyDefaults()});
-  const spotifyApi = new SpotifyWebApi({
-    refreshToken,
-    ...spotifyDefaults(),
-  });
-
-  spotifyApi
-    .refreshAccessToken()
-    .then((data) => {
-      res.status(200).json({
-        accessToken: data.body.accessToken,
-        expiresIn: data.body.expiresIn,
-      });
-    })
-    .catch((err) => {
-      console.log(err);
-      res.status(400).json({ status: err.status });
+app.post("/api/v1/refresh", async ({ body, headers }, res) => {
+  try {
+    const { refreshToken } = body;
+    const spotifyApi = new SpotifyWebApi({
+      refreshToken,
+      ...spotifyDefaults(headers),
     });
+
+    const spotifyApiResponse = await spotifyApi.refreshAccessToken();
+    const { body } = spotifyApiResponse;
+    res.status(200).json({
+      accessToken: body.accessToken,
+      expiresIn: body.expiresIn,
+    });
+  } catch (error) {
+    res.status(200).json({ error: error.message });
+  }
 });
 
-app.post("/api/v1/login", (req, res) => {
-  const code = req.body.code;
-  console.log({...spotifyDefaults()});
-  const spotifyApi = new SpotifyWebApi({
-    ...spotifyDefaults(),
-  });
-  spotifyApi
-    .authorizationCodeGrant(code)
-    .then((data) => {
-      res.status(200).json({
-        accessToken: data.body.access_token,
-        refreshToken: data.body.refresh_token,
-        expiresIn: data.body.expires_in,
-      });
-    })
-    .catch((err) => {
-      console.log(err);
-      res.status(400).json({ status: err.status });
+app.post("/api/v1/login", async ({ body, headers }, res) => {
+  try {
+    const code = body.code;
+    if (!code) return res.status(404).json({ error: "Code yok" });
+
+    const spotifyApi = new SpotifyWebApi({ ...spotifyDefaults(headers) });
+    const spotifyApiResponse = await spotifyApi.authorizationCodeGrant(code);
+
+    const { access_token, refresh_token, expires_in } = spotifyApiResponse.body;
+
+    res.status(200).json({
+      accessToken: access_token,
+      refreshToken: refresh_token,
+      expiresIn: expires_in,
     });
+  } catch (error) {
+    res.status(200).json({ error: error.message });
+  }
 });
 
-app.post("/api/v1/GetLyric", async (req, res) => {
-  const { artist, track_name, track_uri, track_image_url } = req.body;
-  db.query("SELECT lyrics FROM song WHERE track_uri = $1 ", [track_uri])
-    .then((selectData) => {
-      if (selectData.rows[0] && selectData.rows[0].lyrics)
-        res.status(200).json({
-          lyrics: selectData.rows[0].lyrics,
-          status: "Success - Kayıtlı",
-        });
-      else
-        lyricsFinder(artist, track_name)
-          .then((lyrics) => {
-            if (!lyrics)
-              return res.status(200).json({
-                lyrics:
-                  "Şarkı sözleri bulunamadı. Daha sonra tekrar deneyiniz.",
-                status:
-                  "Fail - Sözler yok bir daha bulmayı deneyecek. Bulursa kaydedicek",
-              });
-
-            if (selectData.rows[0]) {
-              db.query(`UPDATE song SET lyrics = $1 WHERE track_uri = $2 `, [
-                lyrics,
-                track_uri,
-              ])
-                .then((data) => {
-                  res.status(200).json({
-                    lyrics: lyrics,
-                    status: "Success - Kayıtlı (Update Etti)",
-                  });
-                })
-                .catch((err) => {
-                  console.log(err);
-                  res.status(200).json({
-                    lyrics:
-                      "Şarkı sözleri bulunamadı. Daha sonra tekrar deneyiniz.",
-                    status: "Fail - Update patladı",
-                  });
-                });
-            } else {
-              db.query(
-                `INSERT INTO song (artist,track,lyrics,track_uri,track_image_url) VALUES($1,$2,$3,$4,$5)`,
-                [artist, track_name, lyrics, track_uri, track_image_url]
-              )
-                .then(() => {
-                  res.status(200).json({
-                    lyrics,
-                    status: "Success - Kayıtlı (Insert Etti)",
-                  });
-                })
-                .catch((err) => {
-                  console.log(err);
-                  res.status(200).json({
-                    lyrics:
-                      "Şarkı sözleri bulunamadı. Daha sonra tekrar deneyiniz.",
-                    status: "Fail - Insert patladı",
-                  });
-                });
-            }
-          })
-          .catch((err) => {
-            console.log(err);
-            res.status(200).json({
-              lyrics: "Şarkı sözleri bulunamadı. Daha sonra tekrar deneyiniz.",
-              status: "Fail - LyricsFinder patladı",
-              message: err.message,
-            });
-          });
-    })
-    .catch((err) => {
+app.post("/api/v1/GetLyric", async ({ body }, res) => {
+  try {
+    const { artist, track_name, track_uri, track_image_url } = body;
+    const seletedSong = await db.query(
+      "SELECT id,lyrics FROM song WHERE track_uri = $1 ",
+      [track_uri]
+    );
+    if (seletedSong.rows[0] && seletedSong.rows[0].lyrics)
       res.status(200).json({
-        lyrics: "Şarkı sözleri bulunamadı. Daha sonra tekrar deneyiniz.",
-        status: "Fail - servis patladı",
-        message: err.message,
+        lyrics: seletedSong.rows[0].lyrics,
+        status: "Success - Kayıtlı",
       });
+
+    const lyrics = await lyricsFinder(artist, track_name);
+    if (seletedSong.rows[0] && lyrics)
+      await db.query(
+        `UPDATE song SET lyrics = $1 WHERE track_uri = $2 `,
+        [lyrics, track_uri]
+      );
+
+    if (!seletedSong.rows[0])
+      await db.query(
+        `INSERT INTO song (artist,track,lyrics,track_uri,track_image_url) VALUES($1,$2,$3,$4,$5)`,
+        [artist, track_name, lyrics, track_uri, track_image_url]
+      );
+
+    res.status(200).json({
+      lyrics:
+        lyrics || "Şarkı sözleri bulunamadı. Daha sonra tekrar deneyiniz.",
+      status: "Success",
     });
+  } catch (error) {
+    res.status(200).json({ error: error.message });
+  }
 });
 
-app.post("/api/v1/GetAllSong", (req, res) => {
-  db.query("SELECT * FROM Song ORDER BY ID desc")
-    .then((data) => {
-      res.status(200).json({
-        data: data.rows,
-        status: "Success",
-        totalCount: data.rows.length,
-      });
-    })
-    .catch((err) => {
-      console.log(err);
-      res.status(400).json({ status: err });
+app.post("/api/v1/GetAllSong", async (req, res) => {
+  try {
+    const songs = await db.query("SELECT * FROM Song ORDER BY ID desc");
+    res.status(200).json({
+      data: songs.rows,
+      status: "Success",
+      totalCount: songs.rows.length,
     });
+  } catch (error) {
+    res.status(200).json({
+      lyrics: "Şarkı sözleri bulunamadı. Daha sonra tekrar deneyiniz.",
+      status: "Fail - Service Error",
+    });
+  }
 });
 
 const PORT = process.env.PORT || 5000;
